f191c0c08dc52119a9fe82dd3916c88405112590
ysou5252 <ysou5252@naver.com>
2021-01-05 21:19:22 +0900

    21.01.06

---
+++ practice2.py
@@ -0,0 +1,13 @@
+python = "Python is Amazing"
+print(python.lower())
+print(python.upper())
+print(python[0].isupper())
+print(len(python))
+print(python.replace("Python","Java"))
+
+index = python.index("n")
+print(index)
+index = python.index("n",index+1)
+print(index)
+
+print(python.find("Java"))
