7d1a0f62d94472be23d83beb9b824d92a4236861
ysou5252 <76947651+ysou5252@users.noreply.github.com>
2022-02-26 00:46:00 +0900

    Delete web_scraping directory

--- web_scraping/5_user_agent.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = "http://nadocoding.tistory.com"
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
-res = requests.get(url, headers=headers)
-with open("nadocoding.html", "w", encoding="utf8") as f:
-    f.write(res.text)
--- web_scraping/7_bs4_webtoons.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "url = https://comic.naver.com/webtoon/list.nhn?titleId=675554"
-res = requests.get(url)
-res.raise_for_status()
-
-soup = BeautifulSoup(res.text, "lxml")
